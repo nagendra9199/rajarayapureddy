7b523abbfeadedde5912a67cb9982f7d241075a5
Nagendra Polisetti <125352965+nagendra9199@users.noreply.github.com>
2023-12-20 17:31:19 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato&display=swap">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Alkatra&display=swap">
+        <meta name="description" content="Your page description here">
+        <meta name="keywords" content="keyword1, keyword2, keyword3">
+
+        <link rel="stylesheet" href="index.css">
+        
+
+
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+
+        <title>Raja Rayapureddy - Official Website</title>
+
+        <style>
+        </style>
+    </head>
+    <body>
+        <div>
+
+            <div class="main-header-container">
+                <div class="logo-container">
+                    <div class="logo-title-container">
+                        <img src="images/mainimage.svg" class="logo-image"/>
+                        <p class="title">Rayapureddy Suribabu (RAJA)</p>
+                    </div>
+                </div>
+                <div class="navbar-container">
+                    <a href=""><p>Home</p></a>
+                    <a href=""><p>About</p></a>
+                    <a href=""><p>Activities</p></a>
+                    <a href=""><p>Gallery</p></a>
+                    <a href=""><p>Contact Us</p></a>
+                </div>
+            </div>
+
+            <div class="home-screen-main-container">
+                <div class="home-screen-container">
+                    <div class="content-main-container">
+                        <h1>When you make a choice, you change the future.</h1>
+                        <div class="paragraph-background-container">
+                            <div class="red-line"></div>
+                            <p>Rayapureddy Raja is at the forefront of building a better tomorrow through effective and compassionate leadership.
+                                 With a focus on innovation and inclusivity, he is shaping a path towards progress, ensuring a brighter and more equitable future for all.
+                            </p>
+                        </div>
+                        <div class="readmore-button-container">
+                            <a href="" class="readmore-button">READ MORE</a>
+                        </div>
+                    </div>
+                    <div class="image-main-container">
+                        <img src="images/mainimage.svg" class="main-image" loading="lazy"/>
+                    </div>
+                    <img src="images/shape1.svg" class="shape1"/>
+                    <img src="images/shape2.svg" class="shape2"/>
+                </div>
+            </div>
+
+            <div class="about-us-main-container">
+                <div class="about-us-container">
+                    <img src="images/withcbn1.jpg" class="aboutus-image" loading="lazy"/>
+                </div>
+                <div class="content-aboutus">
+                    <p class="content-paragraph">The emergence of the new political party, the Telugu Desam Party (TDP), marks a significant chapter in the political landscape. Committed to the principles of transparency, inclusivity, and progress, TDP aspires to be a catalyst for positive change in the region. With a focus on addressing the diverse needs of the people and fostering a culture of collaboration, the party envisions a future where every citizen is empowered to contribute to the growth and development of our society. TDP stands poised to bring fresh perspectives and dynamic leadership to the forefront, dedicated to building a more vibrant and prosperous future for all.</p>
+                </div>
+                
+            </div>
+
+            <div class="">
+
+            </div>
+        </div>
+    </body>
+</html>
